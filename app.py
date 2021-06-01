19aa4f12f00227f16e258917a204afbc2b438631
Arghya <arghyasarkar.joker@gmail.com>
2021-06-01 23:30:50 +0530

    Ready For Deployment

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/docs')
+def docs():
+    return render_template('docs.html')
+
 if __name__=='__main__':
 	app.run(debug=True, port=8000)
